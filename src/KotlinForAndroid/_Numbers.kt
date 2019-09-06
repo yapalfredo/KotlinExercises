c67bcd24686cc7156db0510f9b8cd8de005d0447
al <alfredo.yap@stu.bmcc.cuny.edu>
2019-09-06 18:33:37 -0400

    ds Kotlin Foundation - Numbers

---
+++ src/KotlinForAndroid/_Numbers.kt
@@ -0,0 +1,17 @@
+package KotlinForAndroid
+
+fun main() {
+
+    val a = 12
+    val b = 25
+
+    //basic operators
+    println(a + b)
+    println(b - a)
+    println(a / b)
+    println(b * a)
+    
+    //modulo
+    println(b % a)
+
+}
